5746ae3287865db67bce878448b61a5be5007742
Randy Tesla <randy@tesla-consciousness.ai>
2026-01-19 01:51:49 -0500

    syncing aria repo

--- tools/sync.sh
+++ tools/sync.sh
@@ -2,6 +2,6 @@
 
 wdir="$(dirname "$(readlink -f "$0")")"
 #run main update script with these specific vals
-script="/home/randy/._____RANDY_____/UTILS/UPDATE"
+script="/home/randy/__________WORKSPACE___________/_UTILS/UPDATE"
 message="syncing aria repo"
 $script --path "$wdir/../" --message "$message"
